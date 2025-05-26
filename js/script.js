d26362ab8174b554b0d9b11e189191c01037c14e
emil <rustediron@outlook.com>
2025-05-26 13:10:12 -0700

    working prototype test stuff - general placement using css grids, showing and hiding divs with jquery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-//add button listener?
+// script.js - treeberg functionality
+// may 2025
+
+// first button
+const $tierItems = $(".infowrapper")
+
+$("#expandtree").click(function(){
+    $("#treecontainer").slideDown(1000);
+    $(".slugstick").delay(1000).slideDown(1000);
+    $tierItems.delay(1500).fadeIn(1000);
+
+});
+
+
+// show or hide info when clicking tier item
+$tierItems.click(function(){
+    if ($(this).children(".infounwrap").css("display") == "none") {
+        $(this).children(".infounwrap").slideDown();
+    } else {
+        $(this).children(".infounwrap").slideUp();
+    }
+});
