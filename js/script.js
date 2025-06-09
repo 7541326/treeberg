3cd0f3d20de12d6053ad8c5ce65cccf9927c9aba
emil <rustediron@outlook.com>
2025-06-09 10:13:32 -0700

    organized the js

--- js/script.js
+++ js/script.js
@@ -1,139 +1,22 @@
 // script.js - treeberg functionality
 // may 2025
 
-// sound var
+// VARIABLES --------------------------
+// sound
 var sound = new Howl({
   src: ['./sounds/forest-ambience-26617.mp3'],
   loop: true,
   volume: 0.5,
 });
 
+// elements
+const scrollBottom = $("#scroll-to-bottom");
+const $tierItems = $(".infowrapper");
 
-// --------------------------------------------
-// start of buttons scrolling to bottom and top
-
-const scrollBottom = document.getElementById("scroll-to-bottom");
-
-scrollBottom.addEventListener('click', () => {
-    window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: 'smooth'
-    })
-})
-
-//end of scrolling buttons
-
-// variables
-const $tierItems = $(".infowrapper")
-
-// buttons
-$("#bannerStart").click(function () {
-    console.log("clicked");
-    $("#abovetree").slideUp();
-    $("#treecontainer").slideDown(1000);
-    $(".slugstick").delay(1000).slideDown(1000);
-    $tierItems.delay(1500).fadeIn(1000);
-
-  // load particles
-    particlesJS.load('particles-js', 'experiments/particle/js/particles.json', function() {
-    console.log('callback - particles.js config loaded');});
-
-    // play sound
-    sound.play();
-    console.log("playing");
-});
-
-
-$("#quizExpand").click(function() {
-    console.log("quiz time!");
-    console.log(score, currentQ);
-    $("#treecontainer").slideUp();
-    $("#quiz").delay(1000).slideDown();
-
-    currentQ = 0;
-    score = 0;
-    randomizedQs = shuffle([...quizQs]).slice(0, maxQuizQuestions); 
-    quizStart(currentQ);
-
-    quizStart(currentQ);
-})
-
-
-$("#quizSubmit").click(function () {
-    if (!$('input[name="quiz"]:checked').val()) {
-        alert("Please select an answer!");
-        return;
-    }
-    quizCheck(currentQ);
-    quizNext();
-});
-
-$("#quizExit").click(function () {
-    $("#quiz").slideUp(1000);
-    $("#treecontainer").slideDown(1000);
-    $(".slugstick").delay(1000).slideDown(1000);
-    $tierItems.delay(1500).fadeIn(1000);
-    
-    //hides feedback banner 
-     $("#quizFeedback").hide();
-
-    $("#quizQ").delay(2000).slideDown();
-    $("#quizA").delay(2000).slideDown();
-    $("#quizSubmit").delay(2000).slideDown();
-    $("#quizResults").delay(2000).slideUp();
-    $("#quizExit").delay(2000).slideUp();
-    currentQ = 0;
-    score = 0;
-    console.log(score, currentQ);
-
-});
-
-// show or hide info when clicking tier item
-$tierItems.click(function () {
-    if ($(this).children(".infounwrap").css("display") == "none") {
-        $(this).children(".infounwrap").slideDown();
-    } else {
-        $(this).children(".infounwrap").slideUp();
-    }
-});
-
-// -------------------------------------------
-
-// scrolling
-// init controller
+// scroll initialize
 var controller = new ScrollMagic.Controller();
 
-// adding the things
-// https://scrollmagic.io/examples/basic/reveal_on_scroll.html
-var revealElements = document.getElementsByClassName("infowrapper");
-	for (var i=0; i<revealElements.length; i++) { // create a scene for each element
-		new ScrollMagic.Scene({
-            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
-	        offset: 50,										// start a little later
-			triggerHook: 0.9,
-	})
-	.setClassToggle(revealElements[i], "visible") // add class toggle
-	// .addIndicators({name: "digit " + (i+1) }) // add indicators (requires plugin)
-	.addTo(controller);
-}
-
-
-
-
-//randomizing quiz questions
-// Needs to happen first before any of the other quiz elements
-function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i+1)); // random index
-        [array[i], array[j]] = [array[j], array[i]]; //swap elements
-    }
-    return array;
-}
-
-let randomizedQs = [];
-
-
-//start of implementing our quiz questions
+// quiz
 let quizQs = [
     // please remember that the index starts with "0", not "1"
   {
@@ -238,14 +121,25 @@ let quizQs = [
     ],
     correct: 3//index for last option 
   }
-]
-
-//--------------------------------------------------------------
+];
+let randomizedQs = [];
 let currentQ = 0;
 let score = 0;
+const maxQuizQuestions = 5;
 
-const maxQuizQuestions = 10;
+// ------------------------------------
 
+// FUNCTIONS --------------------------
+// randomize quiz questions
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i+1)); // random index
+        [array[i], array[j]] = [array[j], array[i]]; //swap elements
+    }
+    return array;
+}
+
+// start quiz
 function quizStart(num) {
     $("#quizQ").html(`${randomizedQs[num].question}`);
     $("#quizA").html(``);
@@ -260,6 +154,7 @@ function quizStart(num) {
     $("#quizSubmit").css("display", "block");
 }
 
+// check quiz answers
 function quizCheck(num) {
     let selectedA = $('input[name="quiz"]:checked').val();
     let correctA = randomizedQs[num].correct;
@@ -282,7 +177,7 @@ function quizCheck(num) {
 
 }
 
-
+// go to next question 
 function quizNext() {
     console.log("clicked");
     console.log("Score:", score, "Current Q:", currentQ);
@@ -300,6 +195,99 @@ function quizNext() {
     }
 }
 
+// ------------------------------------
 
+// BUTTONS ----------------------------
+// scroll to bottom
+scrollBottom.click(function() {
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    })
+});
 
-// ---------------------------------------------
+// start button
+$("#bannerStart").click(function () {
+    console.log("clicked");
+    $("#abovetree").slideUp();
+    $("#treecontainer").slideDown(1000);
+    $(".slugstick").delay(1000).slideDown(1000);
+    $tierItems.delay(1500).fadeIn(1000);
+
+    // load particles
+    particlesJS.load('particles-js', 'experiments/particle/js/particles.json', function() {
+    console.log('callback - particles.js config loaded');});
+
+    // play sound
+    sound.play();
+    console.log("playing");
+});
+
+// open quiz
+$("#quizExpand").click(function() {
+    console.log("quiz time!");
+    console.log(score, currentQ);
+    $("#treecontainer").slideUp();
+    $("#quiz").delay(1000).slideDown();
+
+    currentQ = 0;
+    score = 0;
+    randomizedQs = shuffle([...quizQs]).slice(0, maxQuizQuestions); 
+    quizStart(currentQ);
+})
+
+// quiz buttons
+$("#quizSubmit").click(function () {
+    if (!$('input[name="quiz"]:checked').val()) {
+        alert("Please select an answer!");
+        return;
+    }
+    quizCheck(currentQ);
+    quizNext();
+});
+
+$("#quizExit").click(function () {
+    $("#quiz").slideUp(1000);
+    $("#treecontainer").slideDown(1000);
+    $(".slugstick").delay(1000).slideDown(1000);
+    $tierItems.delay(1500).fadeIn(1000);
+    
+    //hides feedback banner 
+     $("#quizFeedback").hide();
+
+    $("#quizQ").delay(2000).slideDown();
+    $("#quizA").delay(2000).slideDown();
+    $("#quizSubmit").delay(2000).slideDown();
+    $("#quizResults").delay(2000).slideUp();
+    $("#quizExit").delay(2000).slideUp();
+    currentQ = 0;
+    score = 0;
+    console.log(score, currentQ);
+
+});
+
+// tier items
+// show or hide info when clicking tier item
+$tierItems.click(function () {
+    if ($(this).children(".infounwrap").css("display") == "none") {
+        $(this).children(".infounwrap").slideDown();
+    } else {
+        $(this).children(".infounwrap").slideUp();
+    }
+});
+
+// -----------------------------------
+
+// SCROLLING -------------------------
+// https://scrollmagic.io/examples/basic/reveal_on_scroll.html
+var revealElements = document.getElementsByClassName("infowrapper");
+	for (var i=0; i<revealElements.length; i++) { // create a scene for each element
+		new ScrollMagic.Scene({
+            triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+	        offset: 50,										// start a little later
+			triggerHook: 0.9,
+	})
+	.setClassToggle(revealElements[i], "visible") // add class toggle
+	// .addIndicators({name: "digit " + (i+1) }) // add indicators (requires plugin)
+	.addTo(controller);
+}
